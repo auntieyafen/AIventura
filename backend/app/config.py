32af38ab5c1081422ef837986c6e2ec06f512071
Tsaichen LO <b09705059@gmail.com>
2025-04-25 14:42:02 +0200

    chore: update config.py and env example

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,6 +2,12 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str
+    google_maps_api_key: str
+    serper_api_key: str
+    serper_endpoint: str
+    openweather_key: str
+    openai_api_key: str
+    openai_api_base: str
 
     class Config:
         env_file = ".env"
